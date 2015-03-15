a8bde6c006d685f42785b2960ca9ab27c1d670d8
Tatsuya Maki <t28oogle@gmail.com>
2015-03-15 19:31:26 +0900

    FeedParserの仮実装をついk追加

---
+++ app/src/main/java/com/t28/android/example/api/parser/FeedParser.java
@@ -0,0 +1,12 @@
+package com.t28.android.example.api.parser;
+
+import com.t28.android.example.data.model.Feed;
+
+public class FeedParser {
+    public FeedParser() {
+    }
+
+    public Feed parse(byte[] data) {
+        return null;
+    }
+}
