71c41324da89e213a0f1ae660358a77bc54e69b4
Tatsuya Maki <t28oogle@gmail.com>
2015-03-15 19:00:45 +0900

    VolleyHolder追加

---
+++ app/src/main/java/com/t28/android/example/volley/VolleyHolder.java
@@ -0,0 +1,32 @@
+package com.t28.android.example.volley;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+public class VolleyHolder {
+    private static RequestQueue sRequestQueue;
+    private static ImageLoader sImageLoader;
+
+    private VolleyHolder() {
+    }
+
+    public static RequestQueue getRequestQueue(Context context) {
+        if (context == null) {
+            throw new NullPointerException("context == null");
+        }
+        if (sRequestQueue == null) {
+            sRequestQueue = Volley.newRequestQueue(context);
+        }
+        return sRequestQueue;
+    }
+
+    public static ImageLoader getImageLoader(Context context) {
+        if (sImageLoader == null) {
+            sImageLoader = new ImageLoader(getRequestQueue(context), null);
+        }
+        return sImageLoader;
+    }
+}
