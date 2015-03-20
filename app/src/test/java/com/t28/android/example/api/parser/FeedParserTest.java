c8c838f2ebe543f625ce80935e67519ec0204555
Tatsuya Maki <t28oogle@gmail.com>
2015-03-20 21:31:42 +0900

    正常系のテストを追加

--- app/src/test/java/com/t28/android/example/api/parser/FeedParserTest.java
+++ app/src/test/java/com/t28/android/example/api/parser/FeedParserTest.java
@@ -1,20 +1,19 @@
 package com.t28.android.example.api.parser;
 
 import android.content.Context;
-import android.content.res.AssetManager;
 import android.os.Build;
 
 import com.t28.android.example.data.model.Feed;
+import com.t28.android.example.test.AssetReader;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,33 +21,63 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(RobolectricTestRunner.class)
 public class FeedParserTest {
 
+    private AssetReader mAssetReader;
+
+    @Before
+    public void setUp() {
+        final Context context = Robolectric.application;
+        mAssetReader = new AssetReader(context.getAssets());
+    }
+
     @Test
     public void parse_shouldReturnFeedWhenResponseHasNoEntry() throws IOException, ParseException {
-        final Context context = Robolectric.application;
-        final AssetManager assetManager = context.getAssets();
-        final InputStream input = assetManager.open("feed_load_success_0.json");
-        final ByteArrayOutputStream output = new ByteArrayOutputStream();
-        final byte[] buffer = new byte[1024];
-        while (input.read(buffer) != -1) {
-            output.write(buffer);
-        }
-
-        final byte[] data = output.toByteArray();
+        // setup
+        final byte[] data = mAssetReader.read("feed_load_success_0.json");
         final FeedParser parser = new FeedParser();
+
+        // exercise
         final Feed feed = parser.parse(data);
 
+        // verify
         assertThat(feed).isNotNull();
     }
 
     @Test
     public void parse_shouldReturnFeedWhenResponseHas1Entry() throws IOException, ParseException {
+        // setup
+        final byte[] data = mAssetReader.read("feed_load_success_1.json");
+        final FeedParser parser = new FeedParser();
+
+        // exercise
+        final Feed feed = parser.parse(data);
+
+        // verify
+        assertThat(feed).isNotNull();
     }
 
     @Test
     public void parse_shouldReturnFeedWhenResponseHas10Entries() throws IOException, ParseException {
+        // setup
+        final byte[] data = mAssetReader.read("feed_load_success_10.json");
+        final FeedParser parser = new FeedParser();
+
+        // exercise
+        final Feed feed = parser.parse(data);
+
+        // verify
+        assertThat(feed).isNotNull();
     }
 
     @Test
     public void parse_shouldReturnFeedWhenResponseHas100Entries() throws IOException, ParseException {
+        // setup
+        final byte[] data = mAssetReader.read("feed_load_success_100.json");
+        final FeedParser parser = new FeedParser();
+
+        // exercise
+        final Feed feed = parser.parse(data);
+
+        // verify
+        assertThat(feed).isNotNull();
     }
 }
