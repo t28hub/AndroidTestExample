f9de38fecda27a385bb553138002163ae3c94fc9
Tatsuya Maki <t28oogle@gmail.com>
2015-03-21 20:12:19 +0900

    MainActivityからRequest関連の処理を削除

--- app/src/main/java/com/t28/android/example/activity/MainActivity.java
+++ app/src/main/java/com/t28/android/example/activity/MainActivity.java
@@ -5,16 +5,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.t28.android.example.R;
-import com.t28.android.example.api.request.FeedRequest;
-import com.t28.android.example.data.model.Feed;
 import com.t28.android.example.fragment.EntryListFragment;
-import com.t28.android.example.volley.VolleyHolder;
-
-import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -30,28 +22,6 @@ public class MainActivity extends ActionBarActivity {
                     .add(android.R.id.content, EntryListFragment.newInstance(FEED_URL))
                     .commit();
         }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        final Request<Feed> request = new FeedRequest.Builder("http://feeds.feedburner.com/hatena/b/hotentry")
-                .setNumber(100)
-                .setSoftTimeToLive(TimeUnit.MINUTES.toMillis(10))
-                .setTimeToLive(TimeUnit.HOURS.toMillis(1))
-                .setListener(new Response.Listener<Feed>() {
-                    @Override
-                    public void onResponse(Feed response) {
-                    }
-                })
-                .setErrorListener(new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                    }
-                })
-                .build();
-        VolleyHolder.getRequestQueue(this).add(request);
     }
 
     @Override
