9bc7a6c1f373ce9d6d3fe1df7a8a39ea843754b8
Tatsuya Maki <t28oogle@gmail.com>
2015-03-21 18:24:18 +0900

    EntryListFragmentの追加

---
+++ app/src/main/java/com/t28/android/example/fragment/EntryListFragment.java
@@ -0,0 +1,18 @@
+package com.t28.android.example.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EntryListFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
