af449271e0143c66468281c99bed3cd87ed75eec
Tatsuya Maki <t28oogle@gmail.com>
2015-03-21 19:06:31 +0900

    デバッグコード追加

--- app/src/main/java/com/t28/android/example/fragment/EntryListFragment.java
+++ app/src/main/java/com/t28/android/example/fragment/EntryListFragment.java
@@ -2,17 +2,42 @@ package com.t28.android.example.fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.t28.android.example.R;
+import com.t28.android.example.view.StatefulFrameLayout;
 
 public class EntryListFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_entry_list, container, false);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getLayout().changeState(StatefulFrameLayout.State.FAILURE);
+            }
+        }, 5000);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getLayout().changeState(StatefulFrameLayout.State.LOADING);
+            }
+        }, 10000);
+    }
+
+    private StatefulFrameLayout getLayout() {
+        return (StatefulFrameLayout) getView();
+    }
 }
