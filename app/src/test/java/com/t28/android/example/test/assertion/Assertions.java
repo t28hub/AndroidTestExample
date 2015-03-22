5aefa8971a74d287f5ff3ca05e1d8f3f48041a8a
Tatsuya Maki <t28oogle@gmail.com>
2015-03-22 13:37:44 +0900

    コンストラクタの宣言とassertThatのstatic化

--- app/src/test/java/com/t28/android/example/test/assertion/Assertions.java
+++ app/src/test/java/com/t28/android/example/test/assertion/Assertions.java
@@ -6,13 +6,16 @@ import android.net.Uri;
  * {@link com.t28.android.example}のテストで利用するアサーションのエントリポイント
  */
 public class Assertions extends org.assertj.core.api.Assertions {
+    private Assertions() {
+    }
+
     /**
      * <code>{@link com.t28.android.example.test.assertion.UriAssert}</code>の生成
      *
      * @param actual 検証する値
      * @return Uriのアサーションオブジェクト
      */
-    public UriAssert assertThat(Uri actual) {
+    public static UriAssert assertThat(Uri actual) {
         return new UriAssert(actual);
     }
 }
