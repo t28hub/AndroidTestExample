2385f6f3639a58519ef962b34d64dfd43dc87b87
Tatsuya Maki <t28oogle@gmail.com>
2015-03-22 14:06:34 +0900

    buildUrlのユニットテストを追加

--- app/src/test/java/com/t28/android/example/api/request/AbsRequestTest.java
+++ app/src/test/java/com/t28/android/example/api/request/AbsRequestTest.java
@@ -1,5 +1,6 @@
 package com.t28.android.example.api.request;
 
+import android.net.Uri;
 import android.os.Build;
 
 import com.android.volley.Request;
@@ -12,12 +13,16 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.t28.android.example.test.assertion.Assertions.assertThat;
 
 @Config(emulateSdk = Build.VERSION_CODES.JELLY_BEAN_MR2, manifest = "src/test/AndroidManifest.xml")
 @RunWith(RobolectricTestRunner.class)
 public class AbsRequestTest {
-    private static final String URL = "http://example.com";
+    private static final String URL = "http://api.example.com/v1/user";
 
     @Test
     public void constructor_shouldReturnInstance() {
@@ -26,6 +31,73 @@ public class AbsRequestTest {
 
         // verify
         assertThat(request).isNotNull();
+    }
+
+    @Test
+    public void buildUrl_shouldReturnUrlWithParameters() {
+        // setup
+        final Map<String, String> params = new HashMap<>();
+        params.put("query", "android");
+
+        // exercise
+        final String urlString = AbsRequest.buildUrl(URL, params);
+
+        // verify
+        assertThat(Uri.parse(urlString))
+                .hasScheme("http")
+                .hasHost("api.example.com")
+                .hasPath("/v1/user")
+                .hasQueryParameter("query", "android");
+    }
+
+    @Test
+    public void buildUrl_shouldReturnUrlWithEmptyParameter() {
+        // exercise
+        final String urlString = AbsRequest.buildUrl(URL, Collections.<String, String>emptyMap());
+
+        // verify
+        assertThat(Uri.parse(urlString))
+                .hasScheme("http")
+                .hasHost("api.example.com")
+                .hasPath("/v1/user");
+    }
+
+    @Test
+    public void buildUrl_shouldNotAppendQueryParameterWhenNameIsEmpty() {
+        // setup
+        final Map<String, String> params = new HashMap<>();
+        params.put("query", "android");
+        params.put("", "empty");
+
+        // exercise
+        final String urlString = AbsRequest.buildUrl(URL, params);
+
+        // verify
+        assertThat(Uri.parse(urlString))
+                .hasScheme("http")
+                .hasHost("api.example.com")
+                .hasPath("/v1/user")
+                .hasQueryParameter("query", "android")
+                .hasNoQueryParameter("");
+    }
+
+    @Test
+    public void buildUrl_shouldNotAppendQueryParameterWhenParameterIsEmpty() {
+        // setup
+        final Map<String, String> params = new HashMap<>();
+        params.put("query", "android");
+        params.put("empty", "");
+
+        // exercise
+        final String urlString = AbsRequest.buildUrl(URL, params);
+
+        // verify
+        assertThat(Uri.parse(urlString))
+                .hasScheme("http")
+                .hasHost("api.example.com")
+                .hasPath("/v1/user")
+                .hasQueryParameter("query", "android")
+                .hasNoQueryParameter("empty");
     }
 
     public static class TestModel implements Model {
