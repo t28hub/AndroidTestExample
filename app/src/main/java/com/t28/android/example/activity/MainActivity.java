879192ba4eeca369c8d17518bbeed75c6ffb17fc
Tatsuya Maki <t28oogle@gmail.com>
2015-03-22 18:16:19 +0900

    ViewPager対応

--- app/src/main/java/com/t28/android/example/activity/MainActivity.java
+++ app/src/main/java/com/t28/android/example/activity/MainActivity.java
@@ -1,31 +1,63 @@
 package com.t28.android.example.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.t28.android.example.R;
+import com.t28.android.example.data.adapter.FragmentAdapter;
 import com.t28.android.example.fragment.EntryListFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 public class MainActivity extends ActionBarActivity {
-    private static final String FEED_URL = "http://feeds.feedburner.com/hatena/b/hotentry";
-    private static final int ENTRY_COUNT = 100;
-
-    private ViewPager mFragmentPager;
+    @InjectView(R.id.main_view_pager)
+    ViewPager mFragmentPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(android.R.id.content, EntryListFragment.newInstance(FEED_URL, ENTRY_COUNT))
-                    .commit();
-        }
+        final List<FragmentAdapter.FragmentCreator> creators = new ArrayList<>();
+        creators.add(new FragmentAdapter.FragmentCreator() {
+            private static final String FEED_URL = "http://feeds.feedburner.com/hatena/b/hotentry";
+
+            @Override
+            public Fragment create() {
+                return EntryListFragment.newInstance(FEED_URL, 50);
+            }
+
+            @Override
+            public CharSequence getTitle() {
+                return FEED_URL;
+            }
+        });
+        creators.add(new FragmentAdapter.FragmentCreator() {
+            private static final String FEED_URL = "http://feeds.feedburner.com/blogspot/hsDu";
+
+            @Override
+            public Fragment create() {
+                return EntryListFragment.newInstance(FEED_URL, 50);
+            }
+
+            @Override
+            public CharSequence getTitle() {
+                return FEED_URL;
+            }
+        });
+        final PagerAdapter adapter = new FragmentAdapter(getSupportFragmentManager(), creators);
+        mFragmentPager.setAdapter(adapter);
     }
 
     @Override
--- app/src/main/java/com/t28/android/example/fragment/EntryListFragment.java
+++ app/src/main/java/com/t28/android/example/fragment/EntryListFragment.java
@@ -1,9 +1,9 @@
 package com.t28.android.example.fragment;
 
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
