a74872f541a090c5c54630ee1a44f72c5f0cfc0e
Tatsuya Maki <t28oogle@gmail.com>
2015-03-22 13:04:09 +0900

    ParseExceptionにコメント追加

--- app/src/main/java/com/t28/android/example/api/parser/ParseException.java
+++ app/src/main/java/com/t28/android/example/api/parser/ParseException.java
@@ -1,12 +1,25 @@
 package com.t28.android.example.api.parser;
 
+/**
+ * パースに失敗した時に送出する例外クラス
+ */
 public class ParseException extends Exception {
     private static final long serialVersionUID = 4241998555954298353L;
 
+    /**
+     * コンストラクタ
+     *
+     * @param message 詳細メッセージ
+     */
     public ParseException(String message) {
         super(message);
     }
 
+    /**
+     * コンストラクタ
+     *
+     * @param throwable 原因となった例外
+     */
     public ParseException(Throwable throwable) {
         super(throwable);
     }
