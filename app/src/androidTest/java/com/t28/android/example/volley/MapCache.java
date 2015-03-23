131ee7c8bfb3d0c99a63b22778ce226d44896a32
Tatsuya Maki <t28oogle@gmail.com>
2015-03-23 13:41:20 +0900

    MapベースのCacheを追加

---
+++ app/src/androidTest/java/com/t28/android/example/volley/MapCache.java
@@ -0,0 +1,55 @@
+package com.t28.android.example.volley;
+
+import com.android.volley.Cache;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapCache implements Cache {
+    private static final long NO_TIME_TO_LIVE = 0;
+
+    private final Map<String, Entry> mEntries;
+
+    public MapCache() {
+        mEntries = new HashMap<>();
+    }
+
+    @Override
+    public Entry get(String key) {
+        return mEntries.get(key);
+    }
+
+    @Override
+    public void put(String key, Entry entry) {
+        mEntries.put(key, entry);
+    }
+
+    @Override
+    public void initialize() {
+        mEntries.clear();
+    }
+
+    @Override
+    public void invalidate(String key, boolean fullExpire) {
+        final Entry entry = mEntries.get(key);
+        if (entry == null) {
+            return;
+        }
+
+        entry.softTtl = NO_TIME_TO_LIVE;
+        if (fullExpire) {
+            entry.ttl = NO_TIME_TO_LIVE;
+        }
+        // 参照を直接操作しているためputしない
+    }
+
+    @Override
+    public void remove(String key) {
+        mEntries.remove(key);
+    }
+
+    @Override
+    public void clear() {
+        mEntries.clear();
+    }
+}
