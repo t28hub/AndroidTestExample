a28ea981e67887bf0fea92ce83f7d9393d17734c
Tatsuya Maki <t28oogle@gmail.com>
2015-03-23 16:53:13 +0900

    StatusCodeにコメント追加と代表的なステータスコードを追加

--- app/src/androidTest/java/com/t28/android/example/volley/StatusCode.java
+++ app/src/androidTest/java/com/t28/android/example/volley/StatusCode.java
@@ -1,14 +1,38 @@
 package com.t28.android.example.volley;
 
+/**
+ * ステータスコード
+ * <p>
+ * NOTE: ApacheのHttpStatusが非推奨になったため本クラスを採用する。
+ * </p>
+ */
 public enum StatusCode {
-    OK(200);
+    OK(200),
+    CREATED(201),
+    ACCEPTED(202),
+    BAD_REQUEST(400),
+    UNAUTHORIZED(401),
+    FORBIDDEN(403),
+    NOT_FOUND(404),
+    INTERNAL_SERVER_ERROR(500),
+    SERVICE_UNAVAILABLE(503);
 
     private final int mCode;
 
+    /**
+     * コンストラクタ
+     *
+     * @param code ステータスコードの整数表現
+     */
     StatusCode(int code) {
         mCode = code;
     }
 
+    /**
+     * 整数表現に変換する
+     *
+     * @return ステータスコードの整数表現
+     */
     public final int toInt() {
         return mCode;
     }
