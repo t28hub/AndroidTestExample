2ae17726cb332af4f188646b6770643161cec462
Tatsuya Maki <t28oogle@gmail.com>
2015-03-29 19:10:58 +0900

    ChildIdMatcherをMatchersからアクセスできるように変更

--- app/src/androidTest/java/com/t28/android/example/test/Matchers.java
+++ app/src/androidTest/java/com/t28/android/example/test/Matchers.java
@@ -1,8 +1,10 @@
 package com.t28.android.example.test;
 
+import android.support.annotation.IdRes;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 
+import com.t28.android.example.test.matcher.ChildIdMatcher;
 import com.t28.android.example.test.matcher.PageMatcher;
 
 import org.hamcrest.Matcher;
@@ -13,5 +15,9 @@ public class Matchers {
 
     public static Matcher<View> atPage(ViewPager pager, int index) {
         return new PageMatcher(pager, index);
+    }
+
+    public static Matcher<View> withChildId(Matcher<View> parentMatcher, @IdRes int childId) {
+        return new ChildIdMatcher(parentMatcher, childId);
     }
 }
